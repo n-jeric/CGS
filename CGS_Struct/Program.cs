218951bb84ab30bf95ac8ae52bec482765fa131c
n-jeric <114877030+n-jeric@users.noreply.github.com>
2022-09-19 15:19:36 -0500

    Add project files.

---
+++ CGS_Struct/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS_Struct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
