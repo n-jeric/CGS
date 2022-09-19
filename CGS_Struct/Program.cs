968c4cdfd55411e8c282850085ec25bd82e8a2c5
n-jeric <114877030+n-jeric@users.noreply.github.com>
2022-09-19 15:47:04 -0500

    Create Structs

--- CGS_Struct/Program.cs
+++ CGS_Struct/Program.cs
@@ -8,11 +8,38 @@ namespace CGS_Struct
 {
     internal class Program
     {
+        #region Structures
+        struct Person
+        {
+            public string fName;
+            public string lName;
+        }
+        struct Curator
+        {
+            public string curatorID;
+            public Person p0;
+            public double commision;
+        }
+        struct Artist
+        {
+            public string artistID;
+            public Person p0;
+        }
+        struct ArtPiece
+        {
+            public string artpieceID;
+            public string curatorID;
+            public string artistID;
+            public string title;
+            public int year;
+            public double value;
+            public double estimate;
+            public char status;
+        }
+        #endregion
         static void Main(string[] args)
         {
-
-
-
+           
         }
     }
 }
