7707d04dd0eb770994cce5f5c54801052aa66437
n-jeric <114877030+n-jeric@users.noreply.github.com>
2022-09-28 13:32:27 -0500

    Create add and verify artist in Gallery

--- CGS_Console/Gallery.cs
+++ CGS_Console/Gallery.cs
@@ -50,5 +50,46 @@ namespace CGS_Console
             myCurators.AddCurator(new Curator(curatorID, firstName, lastName));
             return $"Success! Curator {curatorID} added to the list";
         }
+
+        public bool ArtistVarifier(string aID)
+        {
+            foreach (Artist art in myArtists)
+            {
+                if (art.GetID() == aID)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public string AddArtists()
+        {
+            Console.WriteLine("Please enter ArtistID:");
+            string artistID = Console.ReadLine();
+
+            if (artistID.Length != 5)
+            {
+                return "Error. The artistID should be 5 characters";
+            }
+
+            if (ArtistVarifier(artistID) == true)
+            {
+                return "Error. This ID already exists.";
+            }
+            Console.WriteLine("Please enter first name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please enter last name:");
+            string lastName = Console.ReadLine();
+
+            if (firstName.Length + lastName.Length > 40)
+            {
+                return "Error. Name should be less than 40 characters";
+            }
+            myArtists.AddArtist(new Artist(artistID, firstName, lastName));
+            return $"Success! Artist {artistID} added to the list";
+        }
+
+
+
     }
 }
