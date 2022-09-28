6b299f012eed8f3e3b2065381e4c757d26c5bd9c
n-jeric <114877030+n-jeric@users.noreply.github.com>
2022-09-28 12:01:44 -0500

    Create Indexer classes: Curators, Artists, ArtPieces

---
+++ CGS_Console/Curators.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS_Console
+{
+    class Curators :CollectionBase
+    {
+        public void AddCurator(Curator cur)
+        {
+            List.Add(cur);
+        }
+
+        public Curator this[int index]
+        {
+            get => (Curator)List[index];
+            set => List[index] = value; 
+        }
+    }
+}
