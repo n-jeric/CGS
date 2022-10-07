ef45929a2dd546ad97a794d286910ae85b88101d
n-jeric <114877030+n-jeric@users.noreply.github.com>
2022-10-07 12:09:25 -0500

    Create curator class

---
+++ CGS_WinLibrary/Curator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS_WinLibrary
+{
+    internal class Curator: Person
+    {
+        const double COMMRATE = 0.25;
+        public string CuratorID { get; set; }
+        public double Commission { get; set; }
+
+        public Curator(string curatorID, string firstName, string lastName) : base(firstName, lastName)
+        {
+            CuratorID = curatorID;
+            Commission = 0.0;
+        }
+        public Curator(string curatorID, string firstName, string lastName, string comm) : base(firstName, lastName)
+        {
+            CuratorID = curatorID;
+            Commission = double.Parse(comm);
+        }
+        public override string toString()
+        {
+            return $"{CuratorID} {base.toString()} {Commission}";
+        }
+        public string GetID()
+        {
+            return CuratorID;
+        }
+        //The SetComm method receives the amount eligible for commission for an art piece(this amount is determined by the CalculateComm method 
+        //in ArtPiece). SetComm uses COMMRATE to calculate the 25% commission due and assigns it to the curator identified by the ArtPiece.
+        public void SetComm(double comm)
+        {
+            Commission += (comm * COMMRATE);
+        }
+    }
+}
