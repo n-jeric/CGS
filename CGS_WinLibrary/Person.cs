dd2b9f65b6e35d3b35c5c2c4f1bda5a3a24a3233
n-jeric <114877030+n-jeric@users.noreply.github.com>
2022-10-07 11:23:38 -0500

    Create class person

---
+++ CGS_WinLibrary/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS_WinLibrary
+{
+    class Person
+    {
+        string firstName;
+        string lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void Update(string _fn, string _ln)
+        {
+            this.firstName = _fn;
+            this.lastName = _ln;
+        }
+
+        public virtual string toString()
+        {
+            return $"{this.firstName} {this.lastName}";
+        }
+    }
+}
