889f1abd7375adfd073ca5f77f958dc7bf9fbe60
n-jeric <114877030+n-jeric@users.noreply.github.com>
2022-10-07 12:31:06 -0500

    Create Artist class

---
+++ CGS_WinLibrary/Artist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS_WinLibrary
+{
+    internal class Artist: Person
+    {
+        public string ArtistID { get; set; }
+
+        public Artist(string artistID, string firstName, string lastName) : base(firstName, lastName)
+        {
+            ArtistID = artistID;
+        }
+
+        public string GetID()
+        {
+            return ArtistID;
+        }
+        public override string toString()
+        {
+            return $"{ArtistID} {base.toString()}";
+        }
+    }
+}
