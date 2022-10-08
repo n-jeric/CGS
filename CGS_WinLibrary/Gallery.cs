049533ffab9a9094a69c1994b25594f2cad988c1
n-jeric <114877030+n-jeric@users.noreply.github.com>
2022-10-08 01:28:18 -0500

    add verifier methods

--- CGS_WinLibrary/Gallery.cs
+++ CGS_WinLibrary/Gallery.cs
@@ -8,5 +8,48 @@ namespace CGS_WinLibrary
 {
     internal class Gallery
     {
-    }
+        Curators myCurators = new Curators();
+        Artists myArtists = new Artists();
+        ArtPieces myArtPieces = new ArtPieces();
+
+        public Gallery() { }
+
+        #region Verifier
+        private bool CuratorVerifier(string cID)
+        {
+            foreach (Curator cur in myCurators)
+            {
+                if (cur.GetID() == cID)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool ArtistVarifier(string aID)
+        {
+            foreach (Artist art in myArtists)
+            {
+                if (art.GetID() == aID)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool ArtPieceVarifier(string apID)
+        {
+            foreach (ArtPiece artP in myArtPieces)
+            {
+                if (artP.GetID() == apID)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        #endregion
+
+
+
 }
