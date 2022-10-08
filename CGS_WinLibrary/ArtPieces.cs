0fce793ee4f457e28e2671d7d48731824ffb7d76
n-jeric <114877030+n-jeric@users.noreply.github.com>
2022-10-08 01:06:38 -0500

    Create indexer classes for curator, artist and artpiece

---
+++ CGS_WinLibrary/ArtPieces.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS_WinLibrary
+{
+    class ArtPieces: CollectionBase
+    {
+        public void AddPiece(ArtPiece piece)
+        {
+            List.Add(piece);
+        }
+
+        public ArtPiece this[int index]
+        {
+            get => (ArtPiece)List[index];
+            set => List[index] = value;
+        }
+    }
+}
---
+++ CGS_WinLibrary/Artists.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS_WinLibrary
+{
+    class Artists: CollectionBase
+    {
+        public void AddArtist(Artist artist)
+        {
+            List.Add(artist);
+        }
+
+        public Artist this[int index]
+        {
+            get => (Artist)List[index];
+            set => List[index] = value;
+        }
+    }
+}
